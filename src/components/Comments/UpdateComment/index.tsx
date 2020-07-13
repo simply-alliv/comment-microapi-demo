f85c213f1a29a25719f919ed04a0f0da46c47369
Allistair Vilakazi <45206423+simply-alliv@users.noreply.github.com>
2020-07-13 22:05:38 +0200

    Add vote and update comment tabs (#31)
    * feat: add vote comment tab view
    
    * fix: typo
    
    * feat: add update comment tab view
    
    * fix: update function signature

--- src/components/Comments/UpdateComment/index.tsx
+++ src/components/Comments/UpdateComment/index.tsx
@@ -1,12 +1,20 @@
 import React, { FunctionComponent } from "react";
+import Box from "@material-ui/core/Box";
+import Typography from "@material-ui/core/Typography";
+import TextareaAutosize from "@material-ui/core/TextareaAutosize";
+import Button from "@material-ui/core/Button";
 
 import TabViewIntroSection from "../../TabViewIntroSection";
 
 const updateComponentEndpoints = ["PATCH /comments/:commentId"];
 const updateComponentHeading = "Update a comment";
 const updateComponentSubtitle = "Sometimes we need to make some changes.";
+const defaultText =
+  "Hey, I hope that you are well. I was just looking to confirm our meeting for 10am sharp this morning. Please get back to me as soon as you get this.";
 
 const UpdateComment: FunctionComponent = () => {
+  const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {};
+
   return (
     <React.Fragment>
       <TabViewIntroSection
@@ -14,6 +22,24 @@ const UpdateComment: FunctionComponent = () => {
         heading={updateComponentHeading}
         subtitle={updateComponentSubtitle}
       ></TabViewIntroSection>
+      <Box mt={6}>
+        <Typography variant="body2" align="center" color="textSecondary">
+          What's changed? Write it in the box below.
+        </Typography>
+      </Box>
+      <Box display="flex" flexDirection="column" mt={1}>
+        <TextareaAutosize
+          rowsMin={5}
+          rowsMax={5}
+          defaultValue={defaultText}
+          onChange={handleChange}
+        ></TextareaAutosize>
+      </Box>
+      <Box display="flex" flexDirection="column" alignItems="center" mt={4}>
+        <Button variant="contained" color="secondary">
+          Update Comment
+        </Button>
+      </Box>
     </React.Fragment>
   );
 };
