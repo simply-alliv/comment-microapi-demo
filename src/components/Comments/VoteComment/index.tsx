279faf111dffef2c635195bfcb8d0e0b6f7a4a35
Allistair Vilakazi <allistair.vilakazi@gmail.com>
2020-07-13 21:02:21 +0200

    feat: add vote comment tab view

--- src/components/Comments/VoteComment/index.tsx
+++ src/components/Comments/VoteComment/index.tsx
@@ -1,4 +1,9 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Box from "@material-ui/core/Box";
+import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
 
 import TabViewIntroSection from "../../TabViewIntroSection";
 
@@ -10,7 +15,20 @@ const voteComponentHeading = "Vote a comment";
 const voteComponentSubtitle =
   "Was a really good comment? Show your appreciation.";
 
+const voteTypes = ["Upvote", "Downvote"];
+
+const useStyles = makeStyles({
+  root: {
+    width: "100%",
+    display: "grid",
+  },
+});
+
 const VoteComment: FunctionComponent = () => {
+  const [selectedVoteType, setSelectedVoteType] = useState(voteTypes[0]);
+
+  const classes = useStyles();
+
   return (
     <React.Fragment>
       <TabViewIntroSection
@@ -18,6 +36,32 @@ const VoteComment: FunctionComponent = () => {
         heading={voteComponentHeading}
         subtitle={voteComponentSubtitle}
       ></TabViewIntroSection>
+      <Box mt={6} mb={1}>
+        <Typography variant="body2" align="center" color="textSecondary">
+          Select a vote type you'd like to add to a comment.
+        </Typography>
+      </Box>
+      <Grid container spacing={2}>
+        {voteTypes.map((voteType) => {
+          return (
+            <Grid className={classes.root} item xs={6} key={voteType}>
+              <Button
+                variant={
+                  selectedVoteType === voteType ? "contained" : "outlined"
+                }
+                onClick={() => setSelectedVoteType(voteType)}
+              >
+                {voteType}
+              </Button>
+            </Grid>
+          );
+        })}
+      </Grid>
+      <Box display="flex" flexDirection="column" alignItems="center" mt={4}>
+        <Button variant="contained" color="secondary">
+          Vote Comment
+        </Button>
+      </Box>
     </React.Fragment>
   );
 };
