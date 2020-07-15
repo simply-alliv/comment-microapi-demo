d0c9a265a701f0db389d4b026b7a603a81c4b687
Allistair Vilakazi <allistair.vilakazi@gmail.com>
2020-07-15 03:20:45 +0200

    feat: add mocks

--- src/context/comments/mock.ts
+++ src/context/comments/mock.ts
@@ -1,3 +1,44 @@
 import { Comment } from "../../common/models";
 
-export const mockComments: Comment[] = [];
+export const mockComments: Comment[] = [
+  new Comment(
+    "1edd40c86762e0fb12000003",
+    "owner1@gmail.com",
+    "This is such a lovely day. #BeachParty",
+    95,
+    5,
+    90,
+    1,
+    4
+  ),
+  new Comment(
+    "2edd40c86762e0fb12000003",
+    "owner2@gmail.com",
+    "As some people just don't think. #BeachParty !== #SocialDistancing",
+    55,
+    53,
+    2,
+    0,
+    4
+  ),
+  new Comment(
+    "3edd40c86762e0fb12000003",
+    "owner3@gmail.com",
+    "2Africa will be a game changer for the African digital landscape.",
+    271,
+    255,
+    16,
+    0,
+    4
+  ),
+  new Comment(
+    "4edd40c86762e0fb12000003",
+    "owner4@gmail.com",
+    "Sony is gearing up to change the gaming industry for another generation.",
+    559,
+    372,
+    187,
+    0,
+    4
+  ),
+];
