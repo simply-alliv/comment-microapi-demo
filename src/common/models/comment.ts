596f2e31ec22f4d148a532a2bf4988123e0d2714
Allistair Vilakazi <allistair.vilakazi@gmail.com>
2020-07-15 03:05:58 +0200

    feat: add models' properties

--- src/common/models/comment.ts
+++ src/common/models/comment.ts
@@ -1,4 +1,17 @@
 // Direct representation of the Comment abject from the Comment API
-class Comment {}
+class Comment {
+  constructor(
+    public commentId: string,
+    public refId: string,
+    public ownerId: string,
+    public content: string,
+    public origin: string,
+    public numOfVotes: number,
+    public numOfUpVotes: number,
+    public numOfDownVotes: number,
+    public numOfFlags: number,
+    public numOfReplies: number
+  ) {}
+}
 
 export default Comment;
--- src/common/models/reply.ts
+++ src/common/models/reply.ts
@@ -1,4 +1,15 @@
 // Direct representation of the Reply abject from the Comment API
-class Reply {}
+class Reply {
+  constructor(
+    public replyId: string,
+    public commentId: string,
+    public ownerId: string,
+    public content: string,
+    public numOfVotes: number,
+    public numOfUpVotes: number,
+    public numOfDownVotes: number,
+    public numOfFlags: number
+  ) {}
+}
 
 export default Reply;
